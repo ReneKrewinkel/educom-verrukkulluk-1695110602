0ba90355b43c84671f37a60202a32555f1c0cb9c
SandervBeurden <sandervanbeurden4@outlook.com>
2023-09-25 09:04:22 +0200

    morning update #10

--- index.php
+++ index.php
@@ -6,15 +6,17 @@ require_once("lib/user.php");
 require_once("lib/keukentype.php");
 require_once("lib/gerechtinfo.php");
 require_once("lib/ingredient.php");
+require_once("lib/gerecht.php");
 
 
 /// INIT
 $db = new database();
 $art = new artikel($db->getConnection());
-$user = new user($db->getConnection());
-$keukenType = new keukenType($db->getConnection());
-$gerechtInfo = new gerechtInfo($db->getConnection());
-$ingredient = new ingredient($db->getConnection());
+$userTable = new user($db->getConnection());
+$keukenTypeTable = new keukenType($db->getConnection());
+$gerechtInfoTable = new gerechtInfo($db->getConnection());
+$ingredientTable = new ingredient($db->getConnection());
+$gerechtTable = new gerecht($db->getConnection());
 
 
 /// VERWERK 
@@ -22,12 +24,18 @@ $ingredient = new ingredient($db->getConnection());
 // $info = $gerechtInfo->selecteerInfo(23,$user);
 // $addfavorite = $gerechtInfo->addFavorite(1,2);
 // $deletefavorite = $gerechtInfo->deleteFavorite(59);
-$ingredient = $ingredient->selecteerIngredient(1,$art);
-
+// $ingredient = $ingredient->selecteerIngredient(1,$art);
+$gerecht = $gerechtTable->selecteerGerecht(1);
+$user = $gerechtTable->selecteerUser($userTable,2);
+$ingredient = $gerechtTable->selecteerIngredient($ingredientTable, $art,1);
+$calories = $gerechtTable->calcCalories($ingredientTable, $art,2);
 
 
 /// RETURN
 echo '<pre>';
 
 // var_dump($info);
-var_dump($ingredient);
+// var_dump($gerecht);
+// var_dump($user);
+// var_dump($ingredient);
+var_dump($calories);
---
+++ lib/gerecht.php
@@ -0,0 +1,53 @@
+<?php
+
+class gerecht {
+
+    private $connection;
+
+    public function __construct($connection){
+        $this->connection = $connection;
+    }
+
+
+    public function selecteerGerecht($gerecht_id){
+        $sql = "select * from gerecht where id=$gerecht_id";
+
+        $result = mysqli_query($this->connection,$sql);
+        $gerecht = mysqli_fetch_array($result, MYSQLI_ASSOC);
+
+        return($gerecht);
+
+    }
+
+    public function selecteerUser($user, $gerecht_id){
+        $gerecht = $this->selecteerGerecht($gerecht_id); 
+        $user = $user->selecteerUser($gerecht["user_id"]); 
+        return($user);
+    }
+
+    public function selecteerIngredient($ingredient, $art, $ingredient_id){
+        $ingredient = $ingredient->selecteerIngredient($ingredient_id,$art);
+        return($ingredient);
+
+    }
+
+    public function calcCalories($ingredientTable, $art,$gerecht_id) {
+        $totalCalories = 0;
+        $sql = "select * from ingredient where gerecht_id = $gerecht_id";
+
+        $result = mysqli_query($this->connection,$sql);
+        while($row = mysqli_fetch_assoc($result)) {
+            $ingredient = $ingredientTable->selecteerIngredient($row["id"], $art);
+            $aantal = $ingredient["ingredient"]["aantal"];
+            $verpakking = $ingredient["artikel"]["verpakking"];
+            $caloriesVerpakking = $ingredient["artikel"]["calories"];
+            $totalCalories += ($caloriesVerpakking / $verpakking * $aantal);
+
+        }
+        return($totalCalories);
+
+        
+
+
+    }
+}
